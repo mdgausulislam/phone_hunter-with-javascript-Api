ae7330a2bf55407c6fa9390a0ca90d8b16dc84fb
unknown <gausulislam5@gmail.com>
2023-09-24 12:45:35 +0600

    search button

--- js/app.js
+++ js/app.js
@@ -7,23 +7,39 @@
 
 // loadPhones();
 
-const loadPhones=async()=>{
-    const url=`https://openapi.programming-hero.com/api/phones?search=iphone`;
-    const res=await fetch(url);
-    const data=await res.json();
+const loadPhones = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
     displayPhones(data.data);
 }
 
-const phoneContainer=document.getElementById('phone-container');
-phoneContainer.classList.add('phones');
-const displayPhones=phones=>{
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.innerHTML='';
     // console.log(phones);
-    phones.forEach(phones => {
-        console.log(phones);
+    phones.forEach(phone => {
+        // console.log(phones);
+
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = ` <div class="card h-100 p-4">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">${phone.slug}</p>
+        </div>
+    </div>`;
+        phoneContainer.appendChild(phoneDiv);
     });
 }
 
-
+document.getElementById('btn-search').addEventListener('click',function(){
+    const searchField=document.getElementById('search-field');
+    const searchText=searchField.value;
+    loadPhones(searchText);
+})
 
 
 loadPhones();

2ef080bfecf405585a09c011417811075dc5c333
unknown <gausulislam5@gmail.com>
2023-09-26 15:40:32 +0600

    update section

--- js/app2.js
+++ js/app2.js
@@ -8,12 +8,14 @@ const displayPhones = phones => {
 
     const phoneContainer = document.getElementById('phone-container');
     phones.forEach(phone => {
-        const divContainer = document.createElement('col');
+        const divContainer = document.createElement('div');
+        divContainer.classList.add('col')
         divContainer.innerHTML = `
-        <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <div class="card h-100 p-4 img-fluid">
+        <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">${phone.phone_name
+            }</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                 additional content. This content is a little bit longer.</p>
         </div>

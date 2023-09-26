b8223b796905ec0489a5aaed7635ae6eb9034341
unknown <gausulislam5@gmail.com>
2023-09-26 15:03:10 +0600

    update section

--- js/app2.js
+++ js/app2.js
@@ -1,10 +1,28 @@
-const loadPhones=async()=>{
-    const url=`https://openapi.programming-hero.com/api/phones?search=iphone`;
-    const res=await fetch(url);
-    const data= await res.json();
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
     displayPhones(data.data);
 }
-const displayPhones=data=>{
-    console.log(data);
+const displayPhones = phones => {
+
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        const divContainer = document.createElement('col');
+        divContainer.innerHTML = `
+        <div class="card h-100">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+        </div>
+    </div>
+        `
+        phoneContainer.appendChild(divContainer);
+        console.log(phone);
+    });
 }
+
+
 loadPhones();
